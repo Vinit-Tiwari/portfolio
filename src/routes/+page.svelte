92d40a908500b1c71e10e4ac9570fe656f23cb1b
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2023-06-27 10:22:14 +0530

    updated home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,116 @@
-<span class="flex bg-[#282C33] w-full h-full p-0 m-0 h-full">
-    hi
+<span class="text-white flex flex-col">
+  <span class="flex m-auto p-[2rem] max-w-[70rem] items-center justify-center">
+    <span class="flex flex-col h-full gap-[2rem] items-start">
+      <span class="flex gap-[0.3rem] flex-wrap text-[35px] font-medium">
+        Vinit is a
+        <p class="text-[#C778DD] h-min">full-stack</p>
+        <p class="text-[#C778DD] h-min">developer</p>
+        and
+        <p class="text-[#C778DD] h-min">web</p>
+        <p class="text-[#C778DD] h-min">designer</p>
+      </span>
+
+      <span class="text-[#ABB2BF] text-[20px] max-w-[30rem]">
+        He crafts responsive websites where technologies meet creativity
+      </span>
+
+      <button
+        class="text-[20px] border-2 border-solid border-[#C778DD] py-[0.3rem] px-[0.6rem] hover:bg-[#C778DD] hover:bg-opacity-20"
+      >
+        Contact me !!
+      </button>
+    </span>
+
+    <span class=" flex flex-col items-center gap-[0.2rem]">
+      <span class="relative">
+        <img
+          class="absolute top-[2rem] z-0"
+          width="170"
+          height="170"
+          src="./images/logo1.svg"
+        />
+
+        <img
+          rel="preload"
+          class="relative z-20"
+          src="./images/profilePhoto.svg"
+          alt="profile image"
+        />
+
+        <img
+          class="absolute z-20 top-[15rem] right-[1rem]"
+          src="./images/Dots.svg"
+        />
+      </span>
+
+      <span
+        class="flex items-center gap-[0.3rem] border py-[0.2rem] pl-[1rem] pr-[4rem]"
+      >
+        <div class="w-[1rem] h-[1rem] bg-[#C778DD]" />
+        <span class="text-[18px]"> Currently working on Portfolio </span>
+      </span>
+    </span>
+  </span>
+
+  <span class="flex py-[2rem] items-center">
+    <span class="flex flex-col items-end m-auto">
+      <span class="flex relative border py-[1.5rem] text-[24px] px-[1.2rem]">
+        <svg
+          class="absolute top-[-13px]"
+          width="42"
+          height="29"
+          viewBox="0 0 42 29"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="41.472" height="28.704" fill="#282C33" />
+          <path
+            d="M18.56 19.616C18.56 21.0453 18.0373 22.2507 16.992 23.232C15.968 24.2133 14.72 24.704 13.248 24.704C11.7333 24.704 10.4747 24.2133 9.472 23.232C8.49067 22.2507 8 21.0453 8 19.616C8 18.9333 8.096 18.208 8.288 17.44C8.48 16.672 8.864 15.68 9.44 14.464L14.4 4H19.2L16.544 15.584C17.1413 16.0107 17.6213 16.576 17.984 17.28C18.368 17.9627 18.56 18.7413 18.56 19.616ZM32.8 19.616C32.8 21.0453 32.2773 22.2507 31.232 23.232C30.1867 24.2133 28.9387 24.704 27.488 24.704C26.016 24.704 24.768 24.2133 23.744 23.232C22.7413 22.2507 22.24 21.0453 22.24 19.616C22.24 18.9333 22.336 18.208 22.528 17.44C22.72 16.672 23.1147 15.68 23.712 14.464L28.704 4H33.472L30.784 15.584C31.4027 16.0107 31.8933 16.576 32.256 17.28C32.6187 17.9627 32.8 18.7413 32.8 19.616Z"
+            fill="#ABB2BF"
+          />
+        </svg>
+
+        With great power comes great electricity bill
+        <svg
+          class="absolute left-[85%] top-[80%]"
+          width="42"
+          height="29"
+          viewBox="0 0 42 29"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="41.472" height="28.704" fill="#282C33" />
+          <path
+            d="M18.56 19.616C18.56 21.0453 18.0373 22.2507 16.992 23.232C15.968 24.2133 14.72 24.704 13.248 24.704C11.7333 24.704 10.4747 24.2133 9.472 23.232C8.49067 22.2507 8 21.0453 8 19.616C8 18.9333 8.096 18.208 8.288 17.44C8.48 16.672 8.864 15.68 9.44 14.464L14.4 4H19.2L16.544 15.584C17.1413 16.0107 17.6213 16.576 17.984 17.28C18.368 17.9627 18.56 18.7413 18.56 19.616ZM32.8 19.616C32.8 21.0453 32.2773 22.2507 31.232 23.232C30.1867 24.2133 28.9387 24.704 27.488 24.704C26.016 24.704 24.768 24.2133 23.744 23.232C22.7413 22.2507 22.24 21.0453 22.24 19.616C22.24 18.9333 22.336 18.208 22.528 17.44C22.72 16.672 23.1147 15.68 23.712 14.464L28.704 4H33.472L30.784 15.584C31.4027 16.0107 31.8933 16.576 32.256 17.28C32.6187 17.9627 32.8 18.7413 32.8 19.616Z"
+            fill="#ABB2BF"
+          />
+        </svg>
+      </span>
+
+      <span class="py-[1rem] border text-[20px] px-[1.2rem] text-[#ABB2BF]"
+        >- Dr. Who</span
+      >
+    </span>
+
+    <div class="border-t border-b border-l w-[5rem] h-[5rem]" />
+  </span>
+
+  <span class="flex m-auto w-full max-w-[66rem] p-[1rem]">
+    <span class="flex gap-[1rem] gap-[1rem] items-center w-full justify-between">
+
+      <span class="flex gap-[1rem] items-center">
+        
+        <span class="flex text-[33px]">
+          <p class="text-[#C778DD]">#</p>
+          projects
+        </span>
+  
+        <div class="flex h-[0.1rem] w-[100vw] max-w-[30rem] bg-[#C778DD]"/>
+      
+      </span>
+
+      <button class="flex ">View all ~~></button>
+    </span>
+    
+  </span>
 </span>
