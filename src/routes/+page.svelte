fd4fab30b59238908e65459a70d610f9ad6e37d7
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2023-06-27 13:32:28 +0530

    Completed skills section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -119,9 +119,123 @@
       <Card />
     </span>
 
-    <span class="flex p-[1rem]">
-      <span class="relative flex">
+    <span class="flex flex-col p-[1rem] gap-[1rem]">
+      <span class="flex gap-[1rem] items-center">
+        <span class="flex text-[33px]">
+          <p class="text-[#C778DD]">#</p>
+          skills
+        </span>
 
+        <div class="flex h-[0.1rem] w-[100vw] max-w-[10rem] bg-[#C778DD]" />
+      </span>
+
+      <span class="flex items-center justify-between gap-[1rem]">
+        <span class="flex relative w-full max-w-[25rem] h-[20rem]">
+          <img
+            class="absolute top-[60%] left-[15%] z-0"
+            width="100"
+            height="100"
+            src="./images/logo1.svg"
+          />
+
+          <div
+            class="flex absolute w-[5rem] h-[5rem] left-[65%] border-[#ABB2BF] border-solid border"
+          />
+
+          <img
+            class="absolute top-[10%] left-[5%] z-20"
+            width="70"
+            height="70"
+            src="./images/Dots.svg"
+          />
+
+          <div class="flex absolute w-[3rem] h-[3rem] border-[#ABB2BF] border-solid border left-[80%] top-[70%]"></div>
+
+          <img
+            class="absolute top-[50%] left-[55%] z-20"
+            width="70"
+            height="70"
+            src="./images/Dots.svg"
+          />
+        </span>
+
+        <span>
+          <span class="flex gap-[1rem]">
+            <span class="flex flex-col max-w-[15rem]">
+              <div
+                class="px-[1rem] py-[0.5rem] border-[#ABB2BF] border-solid border"
+              >
+                Languages
+              </div>
+
+              <div
+                class="px-[1rem] py-[0.3rem] text-[#ABB2BF] border-[#ABB2BF] border-solid border"
+              >
+                Javascript, next, react
+              </div>
+            </span>
+
+            <span class="flex flex-col gap-[1rem]">
+              <span class="flex flex-col max-w-[15rem]">
+                <div
+                  class="px-[1rem] py-[0.5rem] border-[#ABB2BF] border-solid border"
+                >
+                  Databases
+                </div>
+
+                <div
+                  class="px-[1rem] py-[0.3rem] text-[#ABB2BF] border-[#ABB2BF] border-solid border"
+                >
+                  Mongo, firebase
+                </div>
+              </span>
+
+              <span class="flex flex-col max-w-[15rem]">
+                <div
+                  class="px-[1rem] py-[0.5rem] border-[#ABB2BF] border-solid border"
+                >
+                  Other
+                </div>
+
+                <div
+                  class="px-[1rem] py-[0.3rem] text-[#ABB2BF] border-[#ABB2BF] border-solid border"
+                >
+                  HTML, CSS, scss
+                </div>
+              </span>
+            </span>
+
+            <span class="flex flex-col gap-[1rem]">
+              <span class="flex flex-col max-w-[15rem]">
+                <div
+                  class="px-[1rem] py-[0.5rem] border-[#ABB2BF] border-solid border"
+                >
+                  Tools
+                </div>
+
+                <div
+                  class="px-[1rem] py-[0.3rem] text-[#ABB2BF] border-[#ABB2BF] border-solid border"
+                >
+                  VSCode, Figma, Github
+                </div>
+              </span>
+
+              <span class="flex flex-col max-w-[15rem]">
+                <div
+                  class="px-[1rem] py-[0.5rem] border-[#ABB2BF] border-solid border"
+                >
+                  Frameworks
+                </div>
+
+                <div
+                  class="px-[1rem] py-[0.3rem] text-[#ABB2BF] border-[#ABB2BF] border-solid border"
+                >
+                  React vue svelte next
+                </div>
+              </span>
+            </span>
+          </span>
+        </span>
       </span>
     </span>
   </span>
