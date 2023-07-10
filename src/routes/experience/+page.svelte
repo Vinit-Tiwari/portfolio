7de3a0387e1d0858a3731e329e19da9a48a7098f
Vinit-Tiwari <vinitvtiwari0505@gmail.com>
2023-07-10 23:23:22 +0530

    updated experience page

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -22,20 +22,25 @@
         />
       </span>
 
-      <span class="flex flex-col pl-[1rem] gap-[0.5rem]">
+      <span class="flex flex-col lg:pl-[1rem] gap-[0.5rem]">
         <span
           class="flex lg:gap-[1rem] flex-col lg:flex-row lg:items-center justify-between"
         >
-          <span class="flex text-[27px] w-full gap-[1rem] items-center">
+          <span
+            class="flex text-[20px] lg:text-[27px] w-full gap-[1rem] items-center"
+          >
             Web Developer Intern, Avrutti Research
           </span>
 
-          <span class="items-center w-full max-w-[10rem] flex text-[19px]"
+          <span
+            class="items-center pl-[0.5rem] lg:pl-[0rem] w-full max-w-[10rem] flex text-[16px] lg:text-[19px]"
             >10/2022 – 06/2023</span
           >
         </span>
 
-        <ul class="text-[1.1rem] list-disc list-inside">
+        <ul
+          class="text-[1.05rem] lg:text-[1.1rem] list-outside pl-[1rem] list-disc list-inside"
+        >
           <li>
             Built an entire e-commerce website using Nextjs and Firebase as a
             solo project
@@ -53,20 +58,25 @@
         </ul>
       </span>
 
-      <span class="flex flex-col pl-[1rem] gap-[0.5rem]">
+      <span class="flex flex-col lg:pl-[1rem] gap-[0.5rem]">
         <span
           class="flex lg:gap-[1rem] flex-col lg:flex-row lg:items-center justify-between"
         >
-          <span class="flex text-[27px] w-full gap-[1rem] items-center">
+          <span
+            class="flex text-[20px] lg:text-[27px] w-full gap-[1rem] items-center"
+          >
             Front-end Lead, Mindinator
           </span>
 
-          <span class="items-center w-full max-w-[10rem] flex text-[19px]"
+          <span
+            class="items-center pl-[0.5rem] lg:pl-[0rem] w-full max-w-[10rem] flex text-[16px] lg:text-[19px]"
             >11/2022 – 05/2023</span
           >
         </span>
 
-        <ul class="text-[1.1rem] list-disc list-inside">
+        <ul
+          class="text-[1.05rem] lg:text-[1.1rem] pl-[1rem] list-outside list-disc list-inside"
+        >
           <li>
             Built the website - mindinator.com with a team of 3 developers and a
             UI Designer
@@ -98,32 +108,69 @@
       </span>
 
       <ul
-        class="text-[1.1rem] list-disc list-inside flex flex-col p-[1rem] max-w-[66rem] w-full m-auto gap-[1rem]"
+        class="text-[1.05rem] lg:text-[1.1rem] list-outside lg:pl-[1rem] list-disc list-inside flex flex-col p-[1rem] max-w-[66rem] w-full m-auto gap-[1rem]"
       >
         <li>
-          AWS Cloud Virtual Internship, AICTE
-          <ul class="text-[1rem] pl-[1rem] list-inside">
+          <a
+            class="underline decoration-solid flex gap-[0.5rem] items-center"
+            href="https://drive.google.com/file/d/1iAur4r5zT1Yd99giscc792IVv3eix2nI/view"
+          >
+            AWS Cloud Virtual Internship, AICTE 
+          </a>
+          <ul
+            class="text-[0.97rem] lg:text-[1rem] list-disc text-[#ABB2BF] list-outside pl-[1rem] list-inside"
+          >
             <li>
-              → 10 weeks training/internship in AWS Cloud Foundations and Cloud
+              10 weeks training/internship in AWS Cloud Foundations and Cloud
               Architecture
             </li>
 
             <li>
-              → Internship provided by EduSkills in partnership with AICTE and
-              the AWS Academy
+              Internship provided by EduSkills in partnership with AICTE and the
+              AWS Academy
             </li>
           </ul>
         </li>
 
         <li>Google UX Design Professional Certificate</li>
 
-        <li>AWS Fundamentals Specialization</li>
+        <li>
+          <a
+            class="underline decoration-solid flex gap-[0.5rem] items-center"
+            href="https://www.coursera.org/account/accomplishments/specialization/SJKDJPYBR7C2?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=s12n"
+          >
+            AWS Fundamentals Specialization
+            
+          </a>
+        </li>
 
-        <li>Image Processing Specialization by MathWorks</li>
+        <li>
+          <a
+            href="https://www.coursera.org/account/accomplishments/specialization/GVQHTY3KKDMG?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=s12n"
+            class="flex underline decoration-solid gap-[0.5rem] items-center"
+          >
+            Image Processing Specialization by MathWorks
+            
+          </a>
+        </li>
 
-        <li>JavaScript Essentials by NetAcad</li>
+        <li>
+          <a
+            class="underline decoration-solid flex gap-[0.5rem] items-center"
+            href="https://drive.google.com/file/d/1wvFikehRCJO1isSkP3C9Ctdn8n87opNB/view"
+          >
+            JavaScript Essentials by NetAcad
+          </a>
+        </li>
 
-        <li>Python Essentials by NetAcad</li>
+        <li>
+          <a
+            class="underline decoration-solid flex gap-[0.5rem] items-center"
+            href="https://drive.google.com/file/d/1RVJ0MH7r5bto2AOC-RdpKaoX7B1YgFDP/view"
+          >
+            Python Essentials by NetAcad
+          </a>
+        </li>
       </ul>
     </span>
   </span>
